2a20d6daa3778192f04f54773a949332a6da5b2d
yimango <yiwu.wang@gmail.com>
2022-11-06 01:06:30 -0400

    ezpz

--- project/src/App.js
+++ project/src/App.js
@@ -1,22 +1,54 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {Button} from '@mui/material'
+
+import { collection, doc, addDoc, getDoc } from "firebase/firestore"; 
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDj5OgO-GJLetg8TTKDADtBUTNQRVuEds8",
+  authDomain: "newhacks-b144e.firebaseapp.com",
+  projectId: "newhacks-b144e",
+  storageBucket: "newhacks-b144e.appspot.com",
+  messagingSenderId: "737121549886",
+  appId: "1:737121549886:web:e5c6f6efa1743523765b6f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+
+const addlink = async (url, title, body, id) => {
+
+  await addDoc(collection(db, 'articles'), {
+    url: url,
+    title: title,
+    body: body,
+    id : id
+  })
+}
+
+const docRef = doc(db, "articles", "nr69MTOI17jlR6IZNnfQ");
+
+const getData = async (e) => {
+
+  const docSnap = await getDoc(docRef);
+  console.log(docSnap.data().title);
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Button variant = "contained" onClick = {addlink}> Click to kill another brain cell</Button>
+        <Button variant='text' onClick={getData}>get data</Button>
       </header>
     </div>
   );
